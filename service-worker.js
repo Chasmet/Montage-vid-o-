59001646f78be5f0a88f3b5116f3984b49c8c292
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-07-19 21:17:29 +0200

    Mettre à jour le cache hors ligne de l’application

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'remix-studio-v1';
-const ASSETS = ['./', './index.html', './style.css', './js/core.js', './js/editor.js', './js/tracks.js', './js/camera.js', './js/render.js', './js/init.js', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'remix-studio-v2';
+const ASSETS = ['./', './index.html', './style.css', './js/core.js', './js/editor.js', './js/tracks.js', './js/camera.js', './js/render.js', './js/init.js', './js/capcut-ui.js', './js/android-bridge.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
 self.addEventListener('fetch', event => {
